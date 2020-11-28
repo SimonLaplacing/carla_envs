f8067759e58c0fe1d322a83ca5ead0ffa96b538c
SimonLaplacing <540379303@qq.com>
2020-11-28 13:12:16 +0800

    promoted

--- ENVS.py
+++ ENVS.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 try:
-    sys.path.append(glob.glob('D:/CARLA_0.9.10-Pre_Win/WindowsNoEditor/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('E:/CARLA_0.9.10-Pre_Win/WindowsNoEditor/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
@@ -30,9 +30,10 @@ def main():
     actor_list = []
 
     try:
-        # 连接客户端
-        client = carla.Client('localhost', 2000)
-        client.set_timeout(8.0)
+        # 连接客户端:localhost:2000\192.168.199.238:2000
+        # client = carla.Client('localhost', 2000)
+        client = carla.Client('192.168.199.238', 2000) 
+        client.set_timeout(30.0)
 
         # 连接世界
         world = client.load_world('Town04')
@@ -53,14 +54,15 @@ def main():
 
         # 视角设置
         spectator = world.get_spectator()
-        spec_transform = ego.get_transform()
-        spec_transform.location += carla.Location(x=-10,z=10)
+        spec_transform = Transform(Location(x=140.341522, y=-375.140472, z=15.281942), 
+                    Rotation(pitch=0.000000, yaw=0.500910, roll=0.000000))
+        spec_transform.location += carla.Location(x=10,z=10)
         spec_transform.rotation = carla.Rotation(pitch=-45)
         spectator.set_transform(spec_transform)
 
+        # npc设置
         for i in range(1):
             transform.location += carla.Location(x=8)
-
             bp = blueprint_library.find(id='vehicle.lincoln.mkz2017')
             npc = world.try_spawn_actor(bp, transform)
             if npc is None:
@@ -69,11 +71,20 @@ def main():
                 actor_list.append(npc)
                 print('created %s' % npc.type_id)
 
+        # 障碍物设置
+        for i in range(1):
+            transform.location += carla.Location(x=40)
+            bp = random.choice(blueprint_library.filter('vehicle'))
+            obstacle = world.try_spawn_actor(bp, transform)
+            if obstacle is None:
+                print('%s obstacle created failed' % i)
+            else:
+                print('created %s' % obstacle.type_id)
+
         # 车辆控制
         # set_control = carla.VehicleControl(throttle=0.5)
         # ego.apply_control(set_control)
         # npc.apply_control(set_control)
-
         for vehicle in actor_list:
             vehicle.set_autopilot(True)
                 
